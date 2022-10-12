07c6d82eb316d60f195e6a43e1b3d01f2a8c38a1
“Catherine <cathanders10@gmail.com>
2022-10-12 11:53:11 -0400

    created some tables, inserted some values

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -7,7 +9,7 @@ CREATE TABLE users (
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
+    body TEXT NOT NULL,
     author_id INTEGER NOT NULL,
 
     FOREIGN KEY (author_id) REFERENCES users(id)
@@ -21,3 +23,37 @@ CREATE TABLE question_follows (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id-1)
+);
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('Arnob', 'Dam'),
+    ('Cath', 'Anderson')
+
+INSERT INTO
+    questions (title, body, author_id)
+VALUES
+    ('Basic SQL Commands', 'Which command should I use to create a table?', (SELECT id FROM users WHERE fname = 'Cath'))
+    ('Insert SQL Command', 'How do I insert something into a table?', (SELECT id FROM users WHERE fname = 'Arnob'))
+
